a27be316d9de609c87bc8a105290a61e66f4cfdd
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-05 10:57:56 +0300

    first commit

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(string = "Squawk!")
+    puts string
+    string
+  end
+  
+  # Example usage:
+  parrot # Output: Squawk!
+  # => "Squawk!"
+  
+  parrot("Hello, I'm a parrot!") # Output: Hello, I'm a parrot!
+  # => "Hello, I'm a parrot!"
+  
